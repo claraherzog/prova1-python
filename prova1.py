2fe40a10945c2a4a08afe2d8542bef4cfa9cc874
claraherzog <claraherzog@gmail.com>
2024-10-04 10:29:08 -0300

    Add files via upload

---
+++ prova1.py
@@ -0,0 +1,6 @@
+base = float(input("Digite a medida da base do retângulo: "))
+altura = float(input("Digite a medida da altura do retângulo: "))
+
+resultado = base * altura
+
+print(f"A área é de {resultado:.2f}")
